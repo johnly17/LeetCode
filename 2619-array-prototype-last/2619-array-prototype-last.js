06988175c18f12707c9f4483e4dc94ae8f836b8f
John Ly <110739417+johnly17@users.noreply.github.com>
2023-07-31 15:52:45 -0400

    Time: 57 ms (55.04%), Space: 42.2 MB (10.98%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,6 +1,7 @@
 Array.prototype.last = function() {
-    if (this.length === 0) return -1;
-    return this[this.length - 1];
+    // if (this.length === 0) return -1;
+    // return this[this.length - 1];
+    return this.length === 0 ? -1 : this[this.length - 1]
 };
 
 /**
