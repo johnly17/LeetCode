78591bd68daf29586a6ffaf738515ea40fccf6cb
John Ly <110739417+johnly17@users.noreply.github.com>
2023-08-03 20:20:19 -0400

    Time: 54 ms (98.36%), Space: 43.9 MB (42.27%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+//     let result = 0;
+    
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 || i % 5 === 0 || i & 7 === 0) {
+//             result = result + i;
+//         }
+//     }
+//     return result;
+    let sum = 0;
+
+    for (let i = 0; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
+};
