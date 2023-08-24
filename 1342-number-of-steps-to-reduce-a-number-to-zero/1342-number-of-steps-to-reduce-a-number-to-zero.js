963545acafe4b95278519d4b2c23bb65508572f2
John Ly <110739417+johnly17@users.noreply.github.com>
2023-08-24 12:52:08 -0400

    Time: 52 ms (74.73%), Space: 42.1 MB (31.51%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,17 +3,26 @@
  * @return {number}
  */
 var numberOfSteps = function(num) {
+//     let n = num;
+//     let counter = 0;
+//     while (n !== 0) {
+//         if (n % 2 === 0) {
+//             n = n / 2;
+//             counter++;
+//         } else if (n % 2 !== 0) {
+//             n = n - 1;
+//             counter++;
+//         }
+//     }
+    
+//     return counter
     let n = num;
     let counter = 0;
-    while (n !== 0) {
-        if (n % 2 === 0) {
-            n = n / 2;
-            counter++;
-        } else if (n % 2 !== 0) {
-            n = n - 1;
-            counter++;
-        }
+    
+    while (n!==0) {
+        (n%2===0) ? n = n / 2 : n = n -1
+        counter++
     }
     
-    return counter
+    return counter;
 };
