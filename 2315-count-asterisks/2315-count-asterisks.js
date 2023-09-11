476a24011ac81d95627ce915659f5c4c0c332dbd
John Ly <110739417+johnly17@users.noreply.github.com>
2023-09-11 19:44:21 -0400

    Time: 49 ms (80.73%), Space: 42.7 MB (33.03%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+    let green=true, count=0;
+    for(let i=0; i<s.length; i++){
+        if(green && s[i]=="*"){count++};
+        if(s[i]=="|"){green=!green};
+    }
+    return count;
+};
