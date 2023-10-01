cb32e7ab897c39a01cedf577aafa11f2968b9d91
John Ly <110739417+johnly17@users.noreply.github.com>
2023-10-01 17:29:05 -0400

    Time: 53 ms (44.29%), Space: 42.1 MB (57.34%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+    let str = num.toString().split('').sort();
+    
+    return parseInt(str[0] + str[2]) + parseInt(str[1] + str[3]);
+};
