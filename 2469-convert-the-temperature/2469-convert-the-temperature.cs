a4410f81223c1d8c04df4d959645f3df9e7f36af
John Ly <110739417+johnly17@users.noreply.github.com>
2023-10-15 20:48:23 -0400

    Time: 97 ms (5.08%), Space: 36.3 MB (45.49%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cs
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public double[] ConvertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        return new double[] {kelvin, fahrenheit};
+        // double kelvin = ;
+        // double fahrenheit = ;
+        return new double[] {celsius + 273.15, celsius * 1.80 + 32.00};
     }
 }
