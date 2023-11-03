fb605818b8776b84bad2502a79da1addba727a95
John Ly <110739417+johnly17@users.noreply.github.com>
2023-11-03 16:07:36 -0400

    Time: 53 ms (80.82%), Space: 42.4 MB (56.91%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -7,18 +7,9 @@ var differenceOfSums = function(n, m) {
     let num1 = 0;
     let num2 = 0;
     
-    let nArr = [];
-    
     for (let i = 1; i <= n; i++) {
-        nArr.push(i);
-    }
-    
-    for (let j = 0; j < nArr.length; j++) {
-        if (nArr[j] % m === 0) {
-            num2 += nArr[j];
-        } else {
-            num1 += nArr[j];
-        }
+        if (i % m === 0) num2 += i
+        else num1 += i;
     }
     
     return num1 - num2;
